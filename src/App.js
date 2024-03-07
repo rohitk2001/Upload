328e6520d7e5b0e1d73441036b6be41069a9c027
Rohit Khandelwal <rohitkhandelwal2001@gmail.com>
2024-03-07 21:30:14 +0530

    Upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+/* eslint-disable no-unused-expressions */
+import {useState} from "react";
 import './App.css';
+import axios from "axios";
 
 function App() {
+  const [file,setFile] = useState(null);
+  const [progress,setProgress] = useState({started:false, pc:0});
+  const [msg,setMsg] = useState(null)
+
+
+  function handleUpload(){
+    if(!file){
+      console.log("No file selected");
+      return;
+    }
+    const fd = new FormData();
+    fd.append('file',file);
+
+    setMsg("Uploading...");
+    setProgress(prevState => {
+      return {...prevState, started: true}
+    })
+    axios.post('http://127.0.0.1:5000/upload',fd,{
+      onUploadProgress: (progressEvent) => { setProgress(prevState => {
+        return {...prevState,pc:progressEvent.progress*100}
+      })},
+      headers: {
+        "Custom-header": "value",
+      }
+    })
+    .then(res=> {
+      setMsg("Uploaded successful");
+      console.log(res.data);
+    })
+    .catch(err => {
+      setMsg("Uploaded failed");
+      console.log(err)
+    });
+
+    // axios.post('http://localhost:6000/PdfToJSON',fd,{
+    //   onUploadProgress: (progressEvent) => { setProgress(prevState => {
+    //     return {...prevState,pc:progressEvent.progress*100}
+    //   })},
+    //   headers: {
+    //     "Custom-header": "value",
+    //   }
+    // })
+    // .then(res=> {
+    //   setMsg("Uploaded successful");
+    //   console.log(res.data);
+    // })
+    // .catch(err => {
+    //   setMsg("Uploaded failed");
+    //   console.log(err)
+    // });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <input onChange={(e) => {  setFile(e.target.files[0]) }} type="file"/>
+      <button onClick={handleUpload}>Upload</button>
+      { progress.started && <progress max="100" value={progress.pc}></progress>}
+      { msg && <span>{msg}</span>}
     </div>
   );
 }
