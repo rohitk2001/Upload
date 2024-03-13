09c28592cf723dc87606f234736fa80ae31b739a
Rohit Khandelwal <rohitkhandelwal2001@gmail.com>
2024-03-13 18:59:04 +0530

    Added username and password states to store them

--- src/Login.js
+++ src/Login.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import { useState } from "react";
 import './LoginForm.css';
 import { Link } from "react-router-dom";
 
 
 const LoginForm = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    function submit() {
+        console.log(username + " " + password)
+    }
+
     return (
         <div className='wrapper'>
             <form action="">
                 <h3>Login</h3>
                 <div className='input-box'>
-                    <input type="text" placeholder="Username" required />
+                    <input type="text" placeholder="Username" required onChange={(e) => { setUsername(e.target.value) }} />
                 </div>
                 <div className='input-box'>
-                    <input type="password" placeholder="Password" required />
+                    <input type="password" placeholder="Password" required onChange={(e) => { setPassword(e.target.value) }} />
                 </div>
-                <Link to="/tally"><button>Sign In</button></Link>
+                <Link to="/tally"><button onClick={submit}>Sign In</button></Link>
             </form>
         </div>
     );
