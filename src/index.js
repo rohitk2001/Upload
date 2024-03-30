896225b88063e99cc5bce92670126981e134c582
Rohit Khandelwal <rohitkhandelwal2001@gmail.com>
2024-03-30 23:55:20 +0530

    Removed deployment changes

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <HashRouter basename={process.env.PUBLIC_URL}>
-        <App />
-      </HashRouter> 
+    <App />
   </React.StrictMode>
 );
 
